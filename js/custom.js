2f637e044c7f46296e60ac38caebdb6a65055889
Raihan <113172842+Raihanroo@users.noreply.github.com>
2022-10-14 10:01:37 +0300

    creative

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+
+
+var mixer = mixitup('.mixitup_body');
+
+
+
+$(".member-slide").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 200,
+    autoplay: true,
+    dots:true,
+    arrows:false
+
+  })
+
+  $(".customer_slide").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 200,
+    autoplay: true,
+    dots:true,
+    arrows:false
+
+  })
